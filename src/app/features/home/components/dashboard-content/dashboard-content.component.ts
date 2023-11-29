507967091e10131e9b4e13190fcea86d83890866
hzdelgado <88523514+hzdelgado@users.noreply.github.com>
2023-11-29 00:48:59 -0500

    current position box in grid layout done
    unused components removed

--- src/app/features/home/components/dashboard-content/dashboard-content.component.ts
+++ src/app/features/home/components/dashboard-content/dashboard-content.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { UserInfoComponent } from '../user-info/user-info.component';
-import { CurrentPositionComponent } from '../current-position/current-position.component';
-import { InvestmentFundComponent } from '../investment-fund/investment-fund.component';
-
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconService } from 'src/app/core/services/material-icon.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-dashboard-content',
   standalone: true,
-  imports: [UserInfoComponent, CurrentPositionComponent, InvestmentFundComponent],
+  imports: [MatIconModule, HttpClientModule, UserInfoComponent, DatePipe],
+  providers: [MatIconService, MatIconRegistry],
   templateUrl: './dashboard-content.component.html',
   styleUrls: ['./dashboard-content.component.scss']
 })
 export class DashboardContentComponent {
-
+  currentDate = new Date()
+  amount = "S/ 125,528.87"
+  constructor(
+    private service: MatIconService,
+    private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer
+  ) {
+    this.service.registerSvgIcons(this.matIconRegistry, this.domSanitizer);
+   }
 }
